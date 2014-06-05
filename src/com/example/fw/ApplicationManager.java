53e49e74ce1cda397f52a4f518fc729e2699ec14
Lena <etarnovskaya@gmail.com>
2014-06-05 21:31:13 +0300

    Homework 3

--- src/com/example/fw/ApplicationManager.java
+++ src/com/example/fw/ApplicationManager.java
@@ -1,24 +1,24 @@
-package com.example.fw;
-
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-
-public class ApplicationManager {
-	public   WebDriver driver;
-	public   String baseUrl;
+	package com.example.fw;
 	
-	private GroupHelper groupHelper;
-	private ContactHelper contactHelper;
-	private NavigationHelper navigationHelper;
-
-	public  ApplicationManager(){
-	    driver = new FirefoxDriver();
-	    baseUrl = "http://localhost/";
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	    
+	import java.util.concurrent.TimeUnit;
+	
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.firefox.FirefoxDriver;
+	
+	
+	public class ApplicationManager {
+		public   WebDriver driver;
+		public   String baseUrl;
+		
+		private GroupHelper groupHelper;
+		private ContactHelper contactHelper;
+		private NavigationHelper navigationHelper;
+	
+		public  ApplicationManager(){
+		    driver = new FirefoxDriver();
+		    baseUrl = "http://localhost/";
+		    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		    
 	}
 	
 	public void stop() {
--- src/com/example/fw/HelperBase.java
+++ src/com/example/fw/HelperBase.java
@@ -46,8 +46,11 @@ public abstract class HelperBase {
 	  }
 
 	protected void type(By locator, String text) {
+		if (text != null){
 		driver.findElement(locator).clear();
-		driver.findElement(locator).sendKeys(text);
+		driver.findElement(locator).sendKeys(text);	
+		}
+		
 	}
 
 	protected void click(By locator) {
